b0ca138fc3b0f86f79390a8ed5db596f215aa7e6
ZZST487 <sasmitpandey12@gmail.com>
2025-04-18 16:49:41 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -6,3 +6,5 @@ void enqueue(struct queue *q, struct game_state state) {}
 struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
 
 int number_of_moves(struct game_state start) { return 0; }
+
+
